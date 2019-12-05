6412589f51c766a6a3a1dc3941d1e85702f19ae5
Lyn Albacete <lalbacete@innovuze.com>
2019-12-05 14:27:29 +0800

    Renamed files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
-// import App from './App'
+import App from './container/Movies/App'
 
 ReactDOM.render(
-
+  <App path='/' />,
+  
 )
